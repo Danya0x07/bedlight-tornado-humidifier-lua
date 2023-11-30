0a6bff77203426598ae5e69baab88abb2413cfad
Danya0x07 <dlef0xf8@gmail.com>
2023-11-30 20:18:05 +0300

    Add helping functionality to init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+local tornado = require('tornado')
+local sensors = require('sensors')
+local button = require('button')
+
+-- Temporary helpers
+local m = tornado.mist
+local f = tornado.fan
+local g = torbado.gen
